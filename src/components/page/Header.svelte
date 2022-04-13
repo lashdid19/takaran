be6c1b2d0e7a38377e2bd7188020449d16c1f069
lashdid19 <lashdid19@gmail.com>
2022-04-13 10:09:53 +0700

    last commit

--- src/components/page/Header.svelte
+++ src/components/page/Header.svelte
@@ -2,6 +2,6 @@
 
 </script>
 <div class="bg-white border-b border-gray-200 w-full py-3 px-2 md:block">
-    <img src="src/img/takaran.svg" alt="icon" class="w-1/3 hidden lg:block">
-    <img src="src/img/takaran-compact.svg" alt="icon" class="w-1/3 block lg:hidden">
+    <img src="assets/img/takaran.svg" alt="icon" class="w-1/3 hidden lg:block">
+    <img src="assets/img/takaran-compact.svg" alt="icon" class="w-1/3 block lg:hidden">
 </div>
